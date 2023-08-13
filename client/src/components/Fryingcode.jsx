086f06f6a39f8ba1fbbfbfe2b9f20311f98898fb
mohit <mohitbhusal11@gmail.com>
2023-08-13 13:11:03 +0545

    ok

--- client/src/components/Fryingcode.jsx
+++ client/src/components/Fryingcode.jsx
@@ -7,11 +7,12 @@ const Fryingcode = () =>{
     return(
         <div className="full-home">
             <div className="home">
-            <h1>Frying Code</h1>
-            <p>This Application perform CRUD operation using MERN stack</p>
-            <h4>You can perform following task in this appkication:</h4>
-            <p>Add || Update || Delete || View</p>
-        </div>
+                <h1>Frying Code</h1>
+                <p>This Application perform CRUD operation using MERN stack</p>
+                <h4>You can perform following task in this appkication:</h4>
+                <p>Add || Update || Delete || View</p>
+                <h3>Git-Hub: <a href="https://github.com/mohitbhusal11/CRUDusingMERN"> Code </a></h3>
+            </div>
         </div>
     )
 }
